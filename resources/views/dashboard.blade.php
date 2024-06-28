4aa0d3b5c9ffa0505c1205fafb879f6022eafc01
muhammadzulfans <muhammadzulfans932@gmail.com>
2024-06-29 02:41:26 +0700

    cek

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard - Laravel Admin Panel With LOgin and Resgistration')
+
+@section('content')
+    <div class="row">
+        Dashboard
+    </div>
+@endsection
